8b1c7cabb54faf0ae00cf24f6ef3486bf0086054
CamilaCarrenoV <camila.carreno89@gmail.com>
2024-08-27 19:46:06 -0400

    vista de las tarjetas listas

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,9 +1,12 @@
 export const createHeader = ()=> {
     const header = document.createElement('header');
     header.innerHTML = `
-    <div= title>
+    <div class="title">
+    <img class="logo" src="images/logo.png" alt="logo">
     <h1> Dreamland </h1>
+    
     </div>
     `
+
     return header;
 }
--- src/Components/tarjetaPelicula.js
+++ src/Components/tarjetaPelicula.js
@@ -3,20 +3,21 @@ export const cardMovie = (item) => {
     divElement.classList.add("infoMovie");
 
     divElement.innerHTML = `
-    <div>
-    <h1> ${item.name} </h1>
-    <div class="contenedorImagen">
-        <img src="${item.imageUrl}" alt="${item.id}">
+    <div class= movieContent>
+    <h1 class="tittleMovie"> ${item.name} </h1>
+    <div class=imagenText>
+    <div class="imagenMovie">
+        <img class="posterMovie" src="${item.imageUrl}" alt="${item.id}">
       </div>
-      <div class="contenedorTextos">
-        <span>${item.shortDescription}</span> 
-         
-        <span><b>Año de estreno:</b> ${item.facts.estreno}</span> 
-         <span><b>Género:</b> ${item.facts.generoPelicula}</span> 
-         <span><b>Clasificación en IMDb:</b> ${item.facts.clasificPublico}</span> 
-         <span><b>Duración:</b> ${item.facts.tiempoDuracion}</span> 
-         <span> <b>Resumen:</b> ${item.description}</span>
-        <span> <b>¿Sabías qué?: </b> ${item.extraInfo.curiosidades}</span>
+      <div class="infoContent">
+        <span class="textInfo">${item.shortDescription}</span> 
+        <span class="textInfo"><b>Año de estreno:</b> ${item.facts.estreno}</span> 
+         <span class="textInfo"><b>Género:</b> ${item.facts.generoPelicula}</span>
+         <span class="textInfo"><b>Clasificación en IMDb:</b> ${item.facts.clasificPublico}</span> 
+         <span class="textInfo"><b>Duración:</b> ${item.facts.tiempoDuracion}</span> 
+         <span class="textInfo"><b>Resumen:</b> ${item.description}</span>
+        <span class="textInfo"><b>¿Sabías qué?: </b> ${item.extraInfo.curiosidades}</span>
+      </div>
       </div>
     </div>
     `;
